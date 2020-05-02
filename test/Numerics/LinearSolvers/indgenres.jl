7a96cb4d807aa3b5f66f72d95cacb7cf487e0524
sandreza <andrenogueirasouza@gmail.com>
2020-05-02 15:46:58 -0500

    trying to make sense of columnwise solver

--- test/Numerics/LinearSolvers/indgenres.jl
+++ test/Numerics/LinearSolvers/indgenres.jl
@@ -72,9 +72,9 @@ norm(y-b) / norm(b)
 ###
 # MPISTateArray test
 Random.seed!(1235)
-n1 = 3
-n2 = 2
-n3 = 1
+n1 = 8
+n2 = 3
+n3 = 10
 mpi_b = MPIStateArray{T}(mpicomm, ArrayType, n1, n2, n3)
 mpi_x = MPIStateArray{T}(mpicomm, ArrayType, n1, n2, n3)
 mpi_A = ArrayType(randn(n1*n2, n1*n2, n3))
@@ -111,3 +111,66 @@ linear_operator!(mpi_y, mpi_x)
 norm(mpi_y - mpi_b) / norm(mpi_b)
 sol = mpi_A[:,:,1] \ mpi_b[:,:, 1][:]
 norm(sol - mpi_x[:,:,1][:])
+
+###
+# now need to try the columnwise test
+
+# ## More Complex Example
+function closure_linear_operator!(A, tup)
+    function linear_operator!(y, x)
+        alias_x = reshape(x, tup)
+        alias_y = reshape(y, tup)
+        for i6 in 1:tup[6]
+            for i4 in 1:tup[4]
+                for i2 in 1:tup[2]
+                    for i1 in 1:tup[1]
+                        tmp = alias_x[i1, i2, :, i4, :, i6][:]
+                        tmp2 = A[i1, i2, i4, i6] * tmp
+                        alias_y[i1, i2, :, i4, :, i6] .=
+                            reshape(tmp2, (tup[3], tup[5]))
+                    end
+                end
+            end
+        end
+    end
+end
+
+tup = (3, 4, 7, 2, 20, 2)
+
+B = [
+    randn(tup[3] * tup[5], tup[3] * tup[5])
+    for i1 in 1:tup[1], i2 in 1:tup[2], i4 in 1:tup[4], i6 in 1:tup[6]
+]
+columnwise_A = [
+    B[i1, i2, i4, i6] * B[i1, i2, i4, i6]' + 10I
+    for i1 in 1:tup[1], i2 in 1:tup[2], i4 in 1:tup[4], i6 in 1:tup[6]
+]
+columnwise_inv_A = [
+    inv(columnwise_A[i1, i2, i4, i6])
+    for i1 in 1:tup[1], i2 in 1:tup[2], i4 in 1:tup[4], i6 in 1:tup[6]
+]
+columnwise_linear_operator! = closure_linear_operator!(columnwise_A, tup)
+columnwise_inverse_linear_operator! =
+    closure_linear_operator!(columnwise_inv_A, tup)
+
+mpi_tup = (tup[1] * tup[2] * tup[3], tup[4], tup[5] * tup[6])
+b = randn(mpi_tup)
+x = randn(mpi_tup)
+
+reshape_tuple_f = tup
+permute_tuple_f = (3,5,1,4,2,6)
+permute_tuple_b = permute_tuple_f
+tmp_reshape_tuple_b = [tup...]
+permute!(tmp_reshape_tuple_b, [permute_tuple_f...])
+reshape_tuple_b = Tuple(tmp_reshape_tuple_b)
+
+# yeah it isn't pretty, should probably define some convenience function
+# that uses the dg model
+linearsolver = IndGenMinRes(b, ArrayType = ArrayType, m = tup[3]*tup[5], n = tup[1]*tup[2]*tup[4]*tup[6], reshape_tuple_f = reshape_tuple_f, permute_tuple_f = permute_tuple_f, reshape_tuple_b = reshape_tuple_b, permute_tuple_b = permute_tuple_b)
+
+iters = linearsolve!(columnwise_linear_operator!, linearsolver, x, b, max_iters = tup[3]*tup[5])
+x_exact = copy(x)
+columnwise_inverse_linear_operator!(x_exact, b)
+norm(x - x_exact) / norm(x_exact)
+columnwise_linear_operator!(x_exact, x)
+norm(x_exact - b)/ norm(b)
